ecb753df15ab4a49322764b45219fbed07c73b69
AHMETCND <ahmetcnd@AHMETCND-MacBook-Pro.local>
2026-01-29 03:43:12 +0300

    ome.tv skip and haptic arayüzü eklendi

--- server/index.js
+++ server/index.js
@@ -1,25 +1,14 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-// ⬇️ __dirname tanımı
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 
 const app    = express();
 const server = http.createServer(app);
 const io     = new Server(server);
 
-// Statik dosyaları sun
-app.use(express.static(path.join(__dirname, '../public')));
-
-// Ana sayfa
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+// Statik dosyalara servis (index.html, script.js vs. "public" klasöründen)
+app.use(express.static('public'));
 
 // Sunucu PORT ayarı (Render kendi portunu ENV ile atar, yoksa 5000 kullan)
 const PORT = process.env.PORT || 3000;
